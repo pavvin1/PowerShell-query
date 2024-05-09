ceef15d02804ccff51431760b7f675bafb3eedfa
pavvin1 <122422847+pavvin1@users.noreply.github.com>
2024-05-09 14:21:58 +0300

    Add files via upload
    O365 Exchange export users, email, from distribution group.
    
    Installing module
    Connect to cloud
    Export rapport to file on local disk

---
+++ O365 Exchange Export users from ditribution group .ps1
@@ -0,0 +1,6 @@
+﻿Install-Module -Name ExchangeOnlineManagement -AllowClobber -Force -Verbose
+Import-Module ExchangeOnlineManagement
+Get-InstalledModule -Name ExchangeOnlineManagement
+Update-Module -Name ExchangeOnlineManagement -Verbose
+Get-DistributionGroup -ResultSize Unlimited > C:\INST\groups.csv
+Get-DistributionGroupMember -Identity "tooryhm@example.ee" -ResultSize Unlimited | Select Name, PrimarySMTPAddress, RecipientType > C:\INST\tooryhm.txt
