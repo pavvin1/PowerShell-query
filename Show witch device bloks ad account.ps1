de335120d464e81270b1ee1f172fc5256a16ad84
pavvin1 <122422847+pavvin1@users.noreply.github.com>
2024-05-13 10:55:51 +0300

    Add files via upload
    This script shows, what device is blocking AD account.
    Device name and time.

---
+++ Show witch device bloks ad account.ps1
@@ -0,0 +1,6 @@
+﻿$Usr = ‘USER NAME’
+$Pdc = (Get-AdDomain).PDCEmulator
+$ParamsEvn = @{
+‘FilterXPath’ = "*[System[EventID=4740] and EventData[Data[@Name='TargetUserName']='$Usr']]"
+}
+$Evnts | foreach {$_.Properties[1].value + ' ' + $_.TimeCreated}
