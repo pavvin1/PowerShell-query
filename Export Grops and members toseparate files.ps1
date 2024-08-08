be2652dfa5d159776c835190f3f86b2eacec635d
pavvin1 <122422847+pavvin1@users.noreply.github.com>
2024-08-08 11:57:26 +0300

    Add files via upload
    This script Export groups and members inside this group to Separate csv/txt file.

---
+++ Export Grops and members toseparate files.ps1
@@ -0,0 +1,23 @@
+﻿ $groups = Get-ADGroup -filter * -SearchBase "DC=monik,DC=ee"
+
+ForEach ($g in $groups) 
+
+{
+
+$path = "c:\Data\" + $g.Name + ".csv"
+
+Get-ADGroup -Identity $g.Name -Properties * | select name,description | Out-File $path -Append
+
+$results = Get-ADGroupMember -Identity $g.Name -Recursive | Get-ADUser -Properties displayname, name  
+
+ForEach ($r in $results){
+
+New-Object PSObject -Property @{       
+
+    DisplayName = $r.displayname | Out-File $path -Append
+
+  }
+
+}   
+
+} 
